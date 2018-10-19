b46fdbc9bc7fae70eabaca1c5effa40b90838f5a
Sarotta0302 <33473222+Sarotta0302@users.noreply.github.com>
2018-10-19 18:33:11 +0800

    溫溼度圖表

--- BeeBox/index.php
+++ BeeBox/index.php
@@ -220,6 +220,65 @@
 		</div>
 
 	</div><!-- END container-wrap -->
+	
+    <div class="container-wrap">
+		<footer id="fh5co-footer" role="contentinfo">
+			<div class="row">
+				
+				<div class="col-md-6 col-md-push-1">
+				
+					<h4>溫溼度折線圖</h4>
+					  <div class="card-body">
+                          <canvas id="lineChartExample"></canvas>
+                      </div>
+				</div>
+
+				<div class="col-md-5 col-md-push-1">
+					<table class="table table-striped table-hover">
+                      <thead>
+                        <tr>
+                          <th>日期時間<br> Time</th>
+                          <th>溫度<br> Temperature</th>
+                          <th>濕度<br> Humidity</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                          for($i=1;$i<=mysqli_num_rows($tbtnh);$i++)
+                            { $rs=mysqli_fetch_row($tbtnh);
+                        ?><tr>
+                        <td><?php echo $rs[1]?></td>
+                        <td><?php echo $rs[2]?></td>
+                        <td><?php echo $rs[3]?></td>
+                        </tr>
+                        <?php }?>
+                      </tbody>
+                    </table>
+					
+				</div>
+
+				
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+		</footer>
+	</div><!-- END container-wrap -->
 
 	<div class="container-wrap">
 		<footer id="fh5co-footer" role="contentinfo">
@@ -307,9 +366,11 @@
 	<!-- choose hive -->	
     <script src="js/selectbar.js"></script>
     
-    <!-- googlemap -->
+    <!-- googlemap & tnh chart-->
+	<script src="js/Chart.min.js"></script>
     <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAtg4aOaTtIbJ3bkYGoPENDgguwcGBbFNI&sensor=false"></script>
     <script type="text/javascript">
+	/*地圖*/
 	function init_map() {
         /*地圖參數相關設定 Start*/
         var Options = {
@@ -330,7 +391,6 @@
             // The anchor for this image is the base of the flagpole at (0, 32).
             anchor: new google.maps.Point(0, 32)
           };
-
           marker = new google.maps.Marker({
             map: map,
             position: new google.maps.LatLng(<?php echo $La?>,<?php echo $Lo?>), /*圖標經緯度位置*/
@@ -344,6 +404,68 @@
         /*所查詢位置詳細資料 End*/
     }
     google.maps.event.addDomListener(window, 'load', init_map);
+	
+	/*溫溼度*/
+	$(document).ready(
+	function () { 'use strict';
+	    var brandPrimary = 'rgba(51, 179, 90, 1)';
+		var LINECHARTEXMPLE   = $('#lineChartExample');
+		var lineChartExample = new Chart(LINECHARTEXMPLE, {
+        type: 'line',
+            data: {
+                labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+                datasets: [
+                    {
+                        label: "溫度",
+                        fill: true,
+                        lineTension: 0.3,
+                        backgroundColor: "rgba(51, 179, 90, 0.38)",
+                        borderColor: brandPrimary,
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        borderWidth: 1,
+                        pointBorderColor: brandPrimary,
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: brandPrimary,
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+			    		data: [<?php while ($cht = mysqli_fetch_array($tbt)){echo $cht[0].",";}?>],
+                        spanGaps: false
+                    },
+                    {
+                        label: "濕度",
+                        fill: true,
+                        lineTension: 0.3,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        borderWidth: 1,
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [<?php while ($chh = mysqli_fetch_array($tbh)){echo $chh[0].",";}?>],
+                        spanGaps: false
+                    }
+                ]
+            }
+        });
+	
+    });
  </script>
 
 	</body>
